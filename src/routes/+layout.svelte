b46e0c3a4c05fa1dad0b1b2f9a1800a16b7fac92
Pierre Goutheraud <pierre.goutheraud@gmail.com>
2024-12-08 12:39:58 +0100

    website styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import "./global.css";
 	import GithubIcon from "../components/GithubIcon.svelte";
-	import type { Snippet } from "svelte";
+	import { onMount, type Snippet } from "svelte";
 	import { page } from "$app/stores";
 	import Nav from "../www-components/Nav/Nav.svelte";
 	import NavItem from "../www-components/Nav/NavItem.svelte";
@@ -10,12 +10,25 @@
 		children: Snippet;
 	}
 	let { children }: Props = $props();
+	let stars = $state(0);
 
 	const routes = [
 		{ name: "What is it?", pathname: "/" },
 		{ name: "Firestore", pathname: "/firestore" },
 		{ name: "Realtime Database", pathname: "/realtime" }
 	];
+
+	async function fetchStars() {
+		const res = await fetch(
+			"https://api.github.com/repos/pierregoutheraud/svelte-firebase-state"
+		);
+		const data = await res.json();
+		stars = data.stargazers_count;
+	}
+
+	onMount(() => {
+		fetchStars();
+	});
 </script>
 
 <main>
@@ -24,15 +37,15 @@
 			<h1>svelte-firebase-state</h1>
 		</div>
 
-		<p>Svelte 5 utilities for Firebase.</p>
+		<h2>Svelte 5 utilities for Firebase.</h2>
 
 		<div class="install">
 			<div class="console">npm install svelte-firebase-state</div>
 			<a
 				href="https://github.com/pierregoutheraud/svelte-firebase-state"
 				target="_blank"
-				class="source"
-				><GithubIcon /> Source
+				class="github"
+				><GithubIcon /> Github
 			</a>
 			<div class="version">Version {PKG.version}</div>
 		</div>
@@ -78,7 +91,7 @@
 		align-items: center;
 		gap: 14px;
 	}
-	header p {
+	header h2 {
 		font-size: 30px;
 		font-weight: 400;
 	}
@@ -98,16 +111,28 @@
 
 	.source {
 		display: flex;
+		flex-direction: column;
+		gap: 10px;
+		align-items: center;
+	}
+
+	.github {
+		display: flex;
 		justify-content: center;
 		align-items: center;
 		gap: 8px;
+		font-size: 16px;
 		color: black;
 	}
-	.source:hover {
+	a.github span {
+		color: red;
 		text-decoration: none;
 	}
-	.source :global(svg) {
-		width: 30px;
+	.github:hover {
+		text-decoration: none;
+	}
+	.github :global(svg) {
+		width: 24px;
 	}
 
 	.console {
@@ -130,42 +155,11 @@
 		font-size: 40px;
 	}
 
-	main h2 {
-		font-size: 30px;
-		background: var(--black);
-		color: white;
-		padding: 20px;
-	}
-
-	main h3 {
-		font-size: 20px;
-	}
-
 	.docs {
 		display: flex;
 		flex-direction: column;
 		gap: 20px;
 		padding: 40px 0 500px;
 		width: 1000px;
-	}
-
-	.nav {
-		align-self: center;
-		display: flex;
-		gap: 20px;
-	}
-	.nav-item {
-		color: var(--black);
-		text-decoration: none;
-		font-size: 20px;
-		padding: 10px 8px;
-		opacity: 0.8;
-		text-align: center;
-	}
-	.nav-item.active {
-		opacity: 1;
-		font-weight: 600;
-		border-bottom: 2px solid var(--red-1);
-		color: var(--red-1);
 	}
 </style>

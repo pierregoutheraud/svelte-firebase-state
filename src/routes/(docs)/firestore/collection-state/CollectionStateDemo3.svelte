87e23c17e3dc3149a2fa6c175076a5631249c77a
Pierre Goutheraud <pierre.goutheraud@gmail.com>
2024-12-13 18:21:02 +0100

    docs: aggregations

---
+++ src/routes/(docs)/firestore/collection-state/CollectionStateDemo3.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+  import { CollectionState } from "$lib/firestore/CollectionState.svelte.js";
+  import { CollectionAggregateState } from "@/lib/firestore/CollectionAggregateState.svelte.js";
+  import { firestore } from "@/www-lib/firebase.js";
+  import { average, count, sum } from "firebase/firestore";
+
+  type DbUser = {
+    name: string;
+    age: number;
+  };
+
+  type AppUser = DbUser & {
+    id: string;
+  };
+
+  type AggregateData = {
+    count: number;
+    averageAge: number;
+    totalAge: number;
+  };
+
+  const users = new CollectionState<DbUser, AppUser, AggregateData>({
+    firestore,
+    listen: true,
+    path: () => "users_2",
+    aggregate: {
+      count: count(),
+      averageAge: average("age"),
+      totalAge: sum("age")
+    }
+  });
+
+  // OR if you only want the aggregate data
+
+  const aggregateState = new CollectionAggregateState<AggregateData>({
+    firestore,
+    path: () => "users_2",
+    aggregate: {
+      count: count(),
+      averageAge: average("age"),
+      totalAge: sum("age")
+    }
+  });
+
+  // How you can refetch data:
+  function handleRefetch() {
+    // Refetch collection data
+    users.refetch();
+
+    // Refetch aggregate data
+    users.refetch_aggregate_data();
+    aggregateState.refetch();
+  }
+</script>
+
+{JSON.stringify(users.data)}
+<br />
+<br />
+{JSON.stringify(users.aggregateData)}
+<br />
+<br />
+{JSON.stringify(aggregateState.data)}
